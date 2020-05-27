2c314d55bccd0b4094099a70fa843f5c68f3745a
Sharvin1 <sharvin@hotmail.nl>
2020-05-27 16:44:32 +0200

    Some failed JavaScript

--- script/script.js
+++ script/script.js
@@ -15,3 +15,32 @@ function getRandomColor() {
   }
   return color;
 }
+
+/*document.querySelector('#blockHand').addEventListener('click', function (evt) {
+  // Create a blank entity.
+  var newVoxelEl = document.createElement('a-entity');
+
+  // Use the mixin to make it a voxel.
+  newVoxelEl.setAttribute('mixin', 'voxel');
+
+  // Set the position using intersection point. The `snap` component above which
+  // is part of the mixin will snap it to the closest half meter.
+  newVoxelEl.setAttribute('position', evt.detail.intersection.point);
+
+  // Add to the scene with `appendChild`.
+  this.appendChild(newVoxelEl);
+});
+
+function setRandomColor() {
+  var getSpheres = document.getElementByTagNames('a-sphere');
+  for(var i = 0; i < getSpheres.length; i++) {
+    getSpheres.innerHTML = function() {
+      getRandomColor();
+    }
+  }
+}*/
+
+/*  var mouseClick = document.getElementById('mouseCursor');
+  mouseClick.onclick = function() {
+    var makeSphere = document.createElement('a-sphere');
+  }*/
